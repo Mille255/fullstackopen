b615e20a53abbb2b35a42dd567c46948c957fa7c
Mika Huhtala <mika.huhtala1@gmail.com>
2025-10-17 10:37:47 +0300

    1.7 unicafe Step 2: Muutokset App.jsx

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,28 +1,37 @@
 import { useState } from 'react'
 
+
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>
     {text}
   </button>
-) 
+)
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
   
   const handleGoodClick = () => {
     setGood(good + 1)
+    setAll(all + 1)
+    setAverage(average + 1)
     
   }
 
   const handleNeutralClick = () => {
     setNeutral(neutral + 1)
+    setAll(all + 1)
+    setAverage(average + 0)
     
   }
 
   const handleBadClick = () => {
     setBad(bad + 1)
+    setAll(all + 1)
+    setAverage(average - 1)
     
   }
   
@@ -38,6 +47,9 @@ const App = () => {
         <p>good {good}</p>
         <p>neutral {neutral}</p>
         <p>bad {bad}</p>
+        <p>all {all}</p>
+        <p>average {average / all}</p>
+        <p>positive {good / all} %</p>
       </div>
     </div>
   )
