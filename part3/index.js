ebbc3390f80973af2e61e4ce88fec39df348b32f
Mika Huhtala <mika.huhtala1@gmail.com>
2025-10-30 11:40:08 +0200

    3.6 puhelinluettelon backend  step6 muutokset index.js

--- part3/index.js
+++ part3/index.js
@@ -64,8 +64,20 @@ const generateId = () => {
   app.post('/api/persons', (request, response) => {
     const body = request.body
 
+    if (!body.name || !body.number) {
+        return response.status(400).json({ 
+          error: 'name or number is missing' 
+        })
+      }
+
+    if (persons.find(person => person.name === body.name)) {
+        return response.status(400).json({ 
+          error: 'name must be unique' 
+        })
+      } 
+    
     const person = {
-    id: generateId(1000),        
+    id: generateId(),        
     name: body.name,
     number: body.number,
     }
