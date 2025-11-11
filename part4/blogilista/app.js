a7a9ba7751a54fd0963b098ca5bf08c96296fefe
Mika Huhtala <mika.huhtala1@gmail.com>
2025-11-11 12:21:33 +0200

    4.2 blogilista, step2

---
+++ part4/blogilista/app.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const blogsRouter = require('./controllers/blogs')
+
+const app = express()
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI, { family: 4 })
+  .then(() => {
+    logger.info('✅ Connected to MongoDB')
+    })
+    .catch(err => { 
+        logger.error('❌ Error connecting to MongoDB:', err.message)
+    })
+
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
