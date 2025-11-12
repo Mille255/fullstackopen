3dbef05a8a5d79e7d36028ffe285bf056193b72e
Mika Huhtala <mika.huhtala1@gmail.com>
2025-11-12 12:22:33 +0200

    4.13 blogilistan laajennus, step1

--- part4/blogilista/tests/blogs_api.test.js
+++ part4/blogilista/tests/blogs_api.test.js
@@ -1,5 +1,5 @@
 const assert = require('node:assert')
-const { test, after, beforeEach  } = require('node:test')
+const { test, after, beforeEach, describe } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
@@ -8,7 +8,7 @@ const Blog = require('../models/blog')
 
 const api = supertest(app)
 
-
+describe('when there is initially some blogs saved', () => {
 beforeEach(async () => {
   await Blog.deleteMany({})
   let blogObject = new Blog(helper.initialBlogs[0])
@@ -41,6 +41,9 @@ test('returned blogs contain id field', async () => {
   } 
   
 })
+})
+
+describe('addition of a new blog', () => {
 
 
 test('a valid blog can be added ', async () => {
@@ -80,42 +83,32 @@ test('a blog without likes should be set to 0', async () => {
     assert.strictEqual(response.body.likes, 0)
 
 })
+})  
 
-test('blog without title should not be saved', async () => {
-  const newBlog = {
-    author: "Heikki Ojanperä",
-    url: "www.Hipera.fi",
-    likes: 11,
-  }
+describe('Deletion of a blog', () => {
+  beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+    })    
 
-   const response = await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-    
+  test('a blog can be deleted', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+    const blogToDelete = blogsAtStart[0]
 
-    const blogsAtEnd = await helper.blogsInDb()
-    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
+  await api
+    .delete(`/api/blogs/${blogToDelete.id}`)
+    .expect(204)
 
+  const blogsAtEnd = await helper.blogsInDb()
+
+  const contents = blogsAtEnd.map(n => n.title)
+  assert(!contents.includes(blogToDelete.title))
+
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1)
 })
 
-test('blog without url should not be saved', async () => {
-  const newBlog = {
-    title: "Kolmas kurjuus",
-    author: "Heikki Ojanperä",
-    likes: 11,
-  }
 
-   const response = await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-    
-
-    const blogsAtEnd = await helper.blogsInDb()
-    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length)
-
-})
+}) 
 
 after(async () => {
   await mongoose.connection.close()
