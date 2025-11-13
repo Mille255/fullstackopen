96706256c4a3a4253e88e27622a01fdf46feaa50
Mika Huhtala <mika.huhtala1@gmail.com>
2025-11-13 11:26:55 +0200

    4.17 blogilistan laajennus, step5

--- part4/blogilista/controllers/users.js
+++ part4/blogilista/controllers/users.js
@@ -20,9 +20,8 @@ usersRouter.post('/', async (request, response) => {
 
 usersRouter.get('/', async (request, response) => {
   const users = await User
-    .find({})
-    .populate('blogs', { title: 1, author: 1, url: 1 }) // tämä lisää käyttäjän blogit, jos haluat
-  response.json(users)
+    .find({}).populate('blogs', { title: 1, author: 1, url: 1, likes: 1 })
+    response.json(users)
 })
 
 module.exports = usersRouter
