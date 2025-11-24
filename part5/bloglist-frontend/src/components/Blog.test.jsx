e92853b015970597b9907629179eeb00fe929407
Mika Huhtala <mika.huhtala1@gmail.com>
2025-11-24 09:23:34 +0200

    5.14 blogilistan testit, step2

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
-import { test, expect } from 'vitest'
+
 
 test('renders content', () => {
   const blog = {
@@ -8,16 +9,64 @@ test('renders content', () => {
     author: 'Heikki Ojanperä',
     url: 'www.Hipera.fi',
     likes: 11,
-    user: {
-      id: '642f1b2f4f1a25630c5e8b9d',
-      username: 'testuser',
-      name: 'Test User'
+   user: {
+      name: 'TestUser'
     }
   }
 
-  render(<Blog blog={blog} />)
-  screen.debug()
+  const mockUser = {
+    id: '642f1b2f4f1a25630c5e8b9d',
+    username: 'testuser',
+    name: 'TestUser'
+  }
+
+  render(<Blog blog={blog} user={mockUser} />)
+  
   const divs = screen.getAllByText(/Kolmas kurjuus/)
   const visibleDiv = divs.find(div => div.style.display !== 'none')
   expect(visibleDiv).toBeInTheDocument()
 })
+
+
+test('shows url, likes and user when the "view" button is clicked', async () => {
+   const blog = {
+    title: 'Kolmas kurjuus',
+    author: 'Heikki Ojanperä',
+    url: 'www.Hipera.fi',
+    likes: 11,
+    user: {
+      name: 'TestUser'
+    }
+  }
+
+  const mockUser = {
+    id: '642f1b2f4f1a25630c5e8b9d',
+    username: 'testuser',
+    name: 'TestUser'
+  }
+
+  const mockHandler = vi.fn()
+
+  render(
+    <Blog blog={blog} user={mockUser} toggleVisibility={mockHandler} />
+  )
+
+  
+  const user = userEvent.setup()
+
+  screen.debug()
+
+  // ennen napin painamista
+  expect(screen.queryByText('www.Hipera.fi')).not.toBeVisible()
+  expect(screen.queryByText(/likes/)).not.toBeVisible()
+  expect(screen.queryByText('TestUser')).not.toBeVisible()
+
+  const button = screen.getByText('view')
+  screen.debug(button)
+  await user.click(button)
+
+  expect(screen.getByText('www.Hipera.fi')).toBeVisible()
+  expect(screen.getByText(/likes/)).toBeVisible()
+  expect(screen.getByText('TestUser')).toBeVisible()
+})
+  
